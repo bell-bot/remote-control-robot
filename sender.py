da9aed079d6c47659583b2d5564917e9f5d6852b
Bella <57637399+bell-bot@users.noreply.github.com>
2020-02-11 01:30:11 +0000

    Add receiver function

--- sender.py
+++ sender.py
@@ -2,10 +2,17 @@ from microbit import *
 import radio
 radio.on()
 
-#Set to any channel between 0 and 100
+#Set to any channel between 0 and 83
 radio.config(channel=7)
-
+rec = "0"
 while True:
+    
+    new_rec = radio.receive()
+    if new_rec != None:
+        rec = new_rec
+        
+    pin0.write_digital(1)
+    
     x = accelerometer.get_x()
     y = accelerometer.get_y()
     coordinates = str(x) + "," + str(y) 
